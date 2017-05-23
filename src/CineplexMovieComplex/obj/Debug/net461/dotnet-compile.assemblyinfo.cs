07c1b48ed71c946f6817f586480f1067b032e2dc
wejrox <pomy_jamie@hotmail.com>
2017-05-23 12:47:16 +1000

    Re-Created project
    Project was re-created as it was not on framework 4.6.1

--- src/CineplexMovieComplex/obj/Debug/net461/dotnet-compile.assemblyinfo.cs
+++ src/CineplexMovieComplex/obj/Debug/net461/dotnet-compile.assemblyinfo.cs
@@ -2,4 +2,4 @@
 [assembly:System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly:System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly:System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly:System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.5.2")]
+[assembly:System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6.1")]
