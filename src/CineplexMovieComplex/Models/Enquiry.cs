55b5b899ce6d569120439e9f3bfbc6a3da1f8e24
wejrox <pomy_jamie@hotmail.com>
2017-05-24 21:25:52 +1000

    nope

--- src/CineplexMovieComplex/Models/Enquiry.cs
+++ src/CineplexMovieComplex/Models/Enquiry.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace CineplexMovieComplex.Models
 {
-    public partial class Enquiry
+    public class Enquiry
     {
-        public int EnquiryId { get; set; }
-        public string Email { get; set; }
+        [Required, Display(Name ="Your Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name ="Your Email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
         public string Message { get; set; }
     }
 }
