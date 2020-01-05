807e27f527930bb6edd2a707d2267534bfcc5ea0
James McDowell <23255412+wejrox@users.noreply.github.com>
2020-01-05 16:42:06 +1100

    Updated to use vs2017 (#4)
    * Comitting nothing because I'm required too.
    
    * Lorem Ipsum.
    
    * Two brothers.
    
    * Updated database
    
    Added seats, bookings and links to cineplexmovie
    
    * Removed bin folder from repo
    
    * Updated home page
    
    Home page now doesn't display text beside buttons
    
    * Updated design for Viewing session times (button).
    Updated button visuals for viewing details.
    Updated carousel on home page (buttons moved down).
    Created Booking movie seat form, formatted.
    
    * Can only see available seats while booking.
    Renamed MovieBooking class and references to MovieTicket.
    Booking tickets not working. Can place booking, it links up but drop down list not returning selected index.
    
    * Cart booking working, cart remains for 1 hour.
    View cart page working, can't process yet.
    
    * Adding to cart complete.
    
    * Removal from cart working
    
    * Cart link no longer displays if no session is active.
    Cant access cart page if not in a session.
    
    * Fixed some forms
    
    * Added support for SecretManager (Google Login)
    Google Login now working.
    Apply database migration when you try.
    Will also have to add some code to the SecretJson, Ask for it when you do.
    
    * Google login connected to cart.
    Review purchase page completed.
    Removed local login.
    User prompted to google login when trying to purchase cart.
    
    * Cant view cart if not items in cart
    
    * Added form for entering credit card details
    
    * Banners have now been updated and reflect their respective pages.
    
    * Formatted Credit Card table
    
    * CSS Updated and looks pretty mint.
    
    * Credit card processing working.
    Max 5 items in cart.
    Re-Direct to cart full page.
    Fixed bug showing all movie tickets instead of only those in cart.
    Re-aligned design of cart page.
    
    * Goodbye purple.
    
    * Corrected banner links.
    
    * Updated GitIgnore
    
    * Added Client-Side form validation.
    Added support for X-Unit test (dependencies)
    
    * Set up Unit Testing
    
    * Fixed crash on startup (wdt_a2_jamesContext has bad constructor)
    
    * Project migration
    
    * Removed randomly tracked obj files
    
    * Shouldn't be committed
    
    * Added rundown and updated to vs2017
    
    Co-authored-by: ashneelDeo <dashmunn@gmail.com>

---
+++ project-details/project-rundown.md
@@ -0,0 +1,25 @@
+# Cineplex Movie Complex
+A Dotnet MVC application designed to emulate a cinema website with bookings and account creation.
+
+## Reason for creation
+This project was completed during my second year of university as a learning excerices to introduce students to DotNet and the MVC pattern.
+
+## Tech used
+- C#
+- Visual Studio 2015/2017
+
+## Definition of done
+This project will be defined as completed when it:
+- [x] Includes a basic number of navigational pages.
+- [x] Provides a method for users view the currently showing movies.
+- [x] Provides a method for users to book into a movie.
+- [x] Handles faux payment without processing or validation.
+
+### Stretch goals
+There is also a stretch goal which I would like to implement.
+- [x] Integration with the Google API for user accounts.
+
+## Retrospective
+- Setting up this project was tough, and I am still not certain of everything that I did. Dotnet core seems to have a bit of a learning curve.
+- There are a lot of connecting components for set up before beginning to work on the website functionality itself.
+- Even though we used dotnet to implement this, javascript was still used for some functionality. I expected to be able to achieve everything without javascript since it is a C# application.
--- src/CineplexMovieComplex/Models/CreditCardModel.cs
+++ src/CineplexMovieComplex/Models/CreditCardModel.cs
@@ -8,29 +8,32 @@ namespace CineplexMovieComplex.Models
 {
     public class CreditCardModel
     {
-        [CreditCard, Required]
+        [CreditCard(ErrorMessage = "The Credit Card number you have entered is invalid"), Required]
         public string CreditCardNumber { get; set; }
-        [StringLength(3, MinimumLength = 3), Required]
+        [StringLength(4, MinimumLength = 3, ErrorMessage = "CCV must be 3 or 4 numbers"), Required]
         public string CVV { get; set;} 
-        [Required]
+        [Required, StringLength(2)]
         public int ExpiryMonth { get; set; }
-        [Required]
+        [Required, StringLength(2)]
         public int ExpiryYear { get; set; }
-        [StringLength(40), Required]
+        [StringLength(40, MinimumLength = 6, ErrorMessage = "Please ensure you have entered all of your card name"), Required]
         public string NameOnCard { get; set; }
         
         public IEnumerable<string> PosExpiryMonth { get; private set; }
-        public List<string> PosExpiryYear { get; private set; }
+        public IEnumerable<string> PosExpiryYear { get; private set; }
 
         public CreditCardModel()
         {
-            PosExpiryYear = new List<string>();
+            List<string> posyrtmp = new List<string>();
             for (int i = 0; i < 10; i++)
-                PosExpiryYear.Add("0" + i.ToString());
+                posyrtmp.Add("0" + i.ToString());
             for (int i = 10; i < 100; i++)
-                PosExpiryYear.Add(i.ToString());
+                posyrtmp.Add(i.ToString());
 
-            PosExpiryMonth = new List <string> { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11" };
+            // We want it read only
+            PosExpiryYear = posyrtmp as IEnumerable<string>;
+
+            PosExpiryMonth = new List <string> { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
         }
     }
 }
